71cabd532de69fc3cca3331d75bb4dca931f0f4b
Teguh Muhammad Ridwan <teguhmr16@gmail.com>
2021-12-13 08:38:10 +0700

    Initial commit

---
+++ app/src/main/java/com/ai/speechrecognitiontajwid/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.dicoding.speechrecognitiontajwid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowManager
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash_screen)
+
+        val DELAY_START = 2000
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, DELAY_START.toLong())
+    }
+}
